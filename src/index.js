a6cd76223a4d1c79e2f6fe3fb6fc24cd5291c8b8
sagarbansal156 <sagarbansal156@gmail.com>
2022-07-06 12:06:57 +0530

    first

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const route = require('./routes/route.js');
+const { default: mongoose } = require('mongoose');
+const app = express();
+
+app.use(express.json());
+
+
+mongoose.connect("mongodb+srv://SagarBansal:HmMiUeU0MyktjG49@cluster0.3yhyk.mongodb.net/Group57Database?retryWrites=true&w=majority", {
+    useNewUrlParser: true
+})
+    .then(() => console.log("MongoDb is connected"))
+    .catch(err => console.log(err))
+
+
+app.use('/', route)
+
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
+});
