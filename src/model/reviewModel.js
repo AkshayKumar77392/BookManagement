876984529cd8c66fa8869a1875094e9fcef8b2d4
sagarbansal156 <sagarbansal156@gmail.com>
2022-07-06 13:05:42 +0530

    day1part4

---
+++ src/model/reviewModel.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose")
+const objectId = mongoose.Schema.Types.ObjectId
+const reviewSchema = new mongoose.Schema(
+    {
+        bookId: {
+            type: objectId,
+            required: true,
+            ref: "book"
+        },
+        reviewedBy: {
+            type: string,
+            required: true,
+            default: 'Guest',
+            value: { String }
+        },
+        reviewedAt: { type: Date, required: true },
+        rating: {
+            type: Number,
+            //minLen: 1,
+            //maxLen: 5,
+            required: true
+        },
+        review: { type: String },
+        isDeleted: { type: boolean, default: false },
+    }
+);
+
+module.exports = mongoose.model('review', reviewSchema)
