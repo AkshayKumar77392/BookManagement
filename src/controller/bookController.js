3af0517d8b3018a300e10fa5bb571c74ff16df34
sagarbansal156 <sagarbansal156@gmail.com>
2022-07-12 20:38:30 +0530

    create book

--- src/controller/bookController.js
+++ src/controller/bookController.js
@@ -85,7 +85,7 @@ const createBook = async function (req, res) {
 
         const data = await booksModel.create(details)
 
-        res.status(200).send({ status: true, data: data })
+        res.status(201).send({ status: true, message:"Success", data: data })
     }
     catch (err) {
         res.status(500).send({ status: false, msg: err.message });
